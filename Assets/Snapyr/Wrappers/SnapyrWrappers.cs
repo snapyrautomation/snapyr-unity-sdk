5ac5b29725e31962609fd2fc56d47449fd0a8325
paulwsmith <paul.smith@games24x7.com>
2021-04-14 14:43:10 -0400

    Fix missing namespace error for `Snapyr.Wrappers`

---
+++ Assets/Snapyr/Wrappers/SnapyrWrappers.cs
@@ -0,0 +1,5 @@
+// Stub to ensure Snapyr.Wrappers namespace is declared.
+// Entire file contents for SnapyrAndroidWrapper, SnapyriOSWrapper are
+// wrapped in #if directives and so may not be declared.
+// TODO: review #if directive usage 
+namespace Snapyr.Wrappers {}
